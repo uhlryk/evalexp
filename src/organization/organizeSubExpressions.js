4d1f71b46e4fe7ce69ba7462d8796ddece38aa37
Sztompka Krzysztof <sztompka.krzysztof@gmail.com>
2021-01-31 22:36:39 +0100

    organizeSubExpression accept tokennode

--- src/organization/organizeSubExpressions.js
+++ src/organization/organizeSubExpressions.js
@@ -1,26 +1,25 @@
 import organizeType from "./organizeType";
 import tokenizeType from "../tokenization/tokenizeType";
 
-export default function organizeSubExpressions(tokens) {
-    const rootTokenNode = {
-        type: organizeType.EXPRESSION,
-        value: []
-    };
+export default function organizeSubExpressions(rootTokenNode) {
     const subExpressions = [rootTokenNode];
+    const tokens = rootTokenNode.value;
+    rootTokenNode.value= [];
     for(const token of tokens) {
-        const tokenNode = subExpressions[subExpressions.length -1];
+        const currentTokenNode = subExpressions[subExpressions.length -1];
         if(token.type === tokenizeType.BRACKET_OPEN) {
             const newTokenNode = {
                 type: organizeType.EXPRESSION,
                 value: []
             };
-            tokenNode.value.push(newTokenNode);
+            currentTokenNode.value.push(newTokenNode);
             subExpressions.push(newTokenNode);
-
         } else if(token.type === tokenizeType.BRACKET_CLOSE) {
             subExpressions.pop();
+        } else if(token.type === tokenizeType.EXPRESSION) {
+            currentTokenNode.value.push(organizeSubExpressions(token));
         } else {
-            tokenNode.value.push(token);
+            currentTokenNode.value.push(token);
         }
     }
     return rootTokenNode;
--- src/organization/organizeSubExpressions.spec.js
+++ src/organization/organizeSubExpressions.spec.js
@@ -4,18 +4,22 @@ import tokenizeType from "../tokenization/tokenizeType";
 
 describe("organizeSubExpressions", () => {
   it("numA*(numB+numC)+numD", () => {
-    const tokens = [
-      { value: "3", type: tokenizeType.NUMBER },
-      { value: "*", type: tokenizeType.OPERATOR },
-      { value: "(", type: tokenizeType.BRACKET_OPEN },
-      { value: "5", type: tokenizeType.NUMBER },
-      { value: "+", type: tokenizeType.OPERATOR },
-      { value: "7", type: tokenizeType.NUMBER },
-      { value: ")", type: tokenizeType.BRACKET_CLOSE },
-      { value: "+", type: tokenizeType.OPERATOR },
-      { value: "10", type: tokenizeType.NUMBER }
-    ];
-    const result = organizeSubExpressions(tokens);
+    const rootTokenNode = {
+      type: organizeType.EXPRESSION,
+      value: [
+        { value: "3", type: tokenizeType.NUMBER },
+        { value: "*", type: tokenizeType.OPERATOR },
+        { value: "(", type: tokenizeType.BRACKET_OPEN },
+        { value: "5", type: tokenizeType.NUMBER },
+        { value: "+", type: tokenizeType.OPERATOR },
+        { value: "7", type: tokenizeType.NUMBER },
+        { value: ")", type: tokenizeType.BRACKET_CLOSE },
+        { value: "+", type: tokenizeType.OPERATOR },
+        { value: "10", type: tokenizeType.NUMBER }
+      ]
+    };
+
+    const result = organizeSubExpressions(rootTokenNode);
     expect(result).to.be.eql({
       type: organizeType.EXPRESSION,
       value: [
@@ -34,23 +38,86 @@ describe("organizeSubExpressions", () => {
       ]
     });
   });
+  it("numA*(numB+expression[numE + (numF + numG)])+numD", () => {
+    const rootTokenNode = {
+      type: organizeType.EXPRESSION,
+      value: [
+        { value: "3", type: tokenizeType.NUMBER },
+        { value: "*", type: tokenizeType.OPERATOR },
+        { value: "(", type: tokenizeType.BRACKET_OPEN },
+        { value: "5", type: tokenizeType.NUMBER },
+        { value: "+", type: tokenizeType.OPERATOR },
+        {
+          type: tokenizeType.EXPRESSION,
+          value: [
+            { value: "2", type: tokenizeType.NUMBER },
+            { value: "+", type: tokenizeType.OPERATOR },
+            { value: "(", type: tokenizeType.BRACKET_OPEN },
+            { value: "7", type: tokenizeType.NUMBER },
+            { value: "+", type: tokenizeType.OPERATOR },
+            { value: "8", type: tokenizeType.NUMBER },
+            { value: ")", type: tokenizeType.BRACKET_CLOSE }
+          ]
+        },
+        { value: ")", type: tokenizeType.BRACKET_CLOSE },
+        { value: "+", type: tokenizeType.OPERATOR },
+        { value: "10", type: tokenizeType.NUMBER }
+      ]
+    };
+
+    const result = organizeSubExpressions(rootTokenNode);
+    expect(result).to.be.eql({
+      type: organizeType.EXPRESSION,
+      value: [
+        { value: "3", type: tokenizeType.NUMBER },
+        { value: "*", type: tokenizeType.OPERATOR },
+        {
+          type: tokenizeType.EXPRESSION,
+          value: [
+            { value: "5", type: tokenizeType.NUMBER },
+            { value: "+", type: tokenizeType.OPERATOR },
+            {
+              type: tokenizeType.EXPRESSION,
+              value: [
+                { value: "2", type: tokenizeType.NUMBER },
+                { value: "+", type: tokenizeType.OPERATOR },
+                {
+                  type: tokenizeType.EXPRESSION,
+                  value: [
+                    { value: "7", type: tokenizeType.NUMBER },
+                    { value: "+", type: tokenizeType.OPERATOR },
+                    { value: "8", type: tokenizeType.NUMBER }
+                  ]
+                }
+              ]
+            }
+          ]
+        },
+        { value: "+", type: tokenizeType.OPERATOR },
+        { value: "10", type: tokenizeType.NUMBER }
+      ]
+    });
+  });
   it("numA*(numB+(numC+numE))+numD", () => {
-    const tokens = [
-      { value: "3", type: tokenizeType.NUMBER },
-      { value: "*", type: tokenizeType.OPERATOR },
-      { value: "(", type: tokenizeType.BRACKET_OPEN },
-      { value: "5", type: tokenizeType.NUMBER },
-      { value: "+", type: tokenizeType.OPERATOR },
-      { value: "(", type: tokenizeType.BRACKET_OPEN },
-      { value: "7", type: tokenizeType.NUMBER },
-      { value: "+", type: tokenizeType.OPERATOR },
-      { value: "8", type: tokenizeType.NUMBER },
-      { value: ")", type: tokenizeType.BRACKET_CLOSE },
-      { value: ")", type: tokenizeType.BRACKET_CLOSE },
-      { value: "+", type: tokenizeType.OPERATOR },
-      { value: "10", type: tokenizeType.NUMBER }
-    ];
-    const result = organizeSubExpressions(tokens);
+    const rootTokenNode = {
+      type: organizeType.EXPRESSION,
+      value: [
+        { value: "3", type: tokenizeType.NUMBER },
+        { value: "*", type: tokenizeType.OPERATOR },
+        { value: "(", type: tokenizeType.BRACKET_OPEN },
+        { value: "5", type: tokenizeType.NUMBER },
+        { value: "+", type: tokenizeType.OPERATOR },
+        { value: "(", type: tokenizeType.BRACKET_OPEN },
+        { value: "7", type: tokenizeType.NUMBER },
+        { value: "+", type: tokenizeType.OPERATOR },
+        { value: "8", type: tokenizeType.NUMBER },
+        { value: ")", type: tokenizeType.BRACKET_CLOSE },
+        { value: ")", type: tokenizeType.BRACKET_CLOSE },
+        { value: "+", type: tokenizeType.OPERATOR },
+        { value: "10", type: tokenizeType.NUMBER }
+      ]
+    };
+    const result = organizeSubExpressions(rootTokenNode);
     expect(result).to.be.eql({
       type: organizeType.EXPRESSION,
       value: [
