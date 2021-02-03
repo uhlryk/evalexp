8642581fee429e252c3bf8f492f43b4cd5745488
Sztompka Krzysztof <sztompka.krzysztof@gmail.com>
2021-02-03 23:10:12 +0100

    left right operator transform

--- src/tokens/LeftRightOperatorToken.js
+++ src/tokens/LeftRightOperatorToken.js
@@ -1,4 +1,5 @@
 import OperatorToken from "./OperatorToken";
+import ValueToken from "./ValueToken";
 
 
 export default class LeftRightOperatorToken extends OperatorToken {
@@ -11,7 +12,42 @@ export default class LeftRightOperatorToken extends OperatorToken {
         this.addChildAtIndex(child, 1);
     }
 
-    transformOperator() {
+    transformLeftOperand() {
+        const operand = this.getLeft();
+        if(!operand) {
+            throw SyntaxError("Left operand is required");
+        }
+        if(!(operand instanceof ValueToken)) {
+            throw SyntaxError("Left operand Should be an instance of ValueToken");
+        }
+        const leftOperandSibling = operand.getLeft();
+        operand.setParent(this);
+        operand.setLeft(null);
+        operand.setRight(null);
+        this.setLeftChild(operand);
+        leftOperandSibling.setRight(this);
+        this.setLeft(leftOperandSibling);
+    }
 
+    transformRightOperand() {
+        const operand = this.getRight();
+        if(!operand) {
+            throw SyntaxError("Right operand is required");
+        }
+        if(!(operand instanceof ValueToken)) {
+            throw SyntaxError("Right operand Should be an instance of ValueToken");
+        }
+        const rightOperandSibling = operand.getRight();
+        operand.setParent(this);
+        operand.setLeft(null);
+        operand.setRight(null);
+        this.setRightChild(operand);
+        rightOperandSibling.setLeft(this);
+        this.setRight(rightOperandSibling);
+    }
+
+    transform() {
+        this.transformLeftOperand();
+        this.transformRightOperand();
     }
 }
